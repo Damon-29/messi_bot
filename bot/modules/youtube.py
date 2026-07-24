1aa1eaa961094c5d6bd81b366470e522e692be1f
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-25 03:22:28 +0530

    Update youtube.py

--- bot/modules/youtube.py
+++ bot/modules/youtube.py
@@ -1,20 +1,37 @@
 import feedparser
 
-from bot.modules.rss import RSSModule
+from bot.models.post import Post
+from bot.modules.base import BaseModule
 
 
-class YouTubeModule(RSSModule):
+class YouTubeModule(BaseModule):
     def __init__(self, channel_id: str):
-        self.channel_id = channel_id
-
-        url = f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
-
-        super().__init__(
-            url=url,
-            source="youtube",
+        self.url = (
+            f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
         )
 
-    def debug(self):
+    def fetch(self):
         feed = feedparser.parse(self.url)
 
-        print(feed.entries[0])
+        posts = []
+
+        for entry in feed.entries:
+            thumbnail = ""
+
+            if "media_thumbnail" in entry:
+                thumbnail = entry.media_thumbnail[0]["url"]
+
+            posts.append(
+                Post(
+                    id=entry.yt_videoid,
+                    source="youtube",
+                    title=entry.title,
+                    url=entry.link,
+                    body=entry.summary,
+                    author=entry.author,
+                    timestamp=entry.published,
+                    thumbnail=thumbnail,
+                )
+            )
+
+        return posts
