1f2a3ed54b90f9e026aa5169ab043e7fe8857e1c
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-25 01:58:33 +0530

    Update post.py

--- bot/models/post.py
+++ bot/models/post.py
@@ -1,1 +1,20 @@
+from dataclasses import dataclass, field
+from typing import Any
 
+@dataclass
+class Post:
+    id: str
+    source: str
+
+    title: str
+    url: str
+
+    body: str = ""
+
+    images: list[str] = field(default_factory=list)
+    thumbnail: str = ""
+
+    author: str = ""
+    timestamp: str = ""
+
+    metadata: dict[str, Any] = field(default_factory=dict)
