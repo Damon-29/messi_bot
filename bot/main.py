dbaa22617037407a57dfe74870be1a36d569ed94
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-25 03:04:13 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,27 +1,15 @@
+from bot.bot import Bot
 from bot.modules.rss import RSSModule
-from bot.state import exists, add
-from bot.services.discord import send_post
 
 
 def main():
-    rss = RSSModule("https://feeds.feedburner.com/TheHackersNews")
+    bot = Bot(
+        modules=[
+            RSSModule("https://feeds.feedburner.com/TheHackersNews"),
+        ]
+    )
 
-    posts = rss.fetch()
-
-    new_posts = 0
-
-    for post in posts:
-        if exists("rss", post.id):
-            print(f"⏩ Skipping: {post.title}")
-            continue
-
-        print(f"📨 Sending: {post.title}")
-
-        send_post(post)
-        add("rss", post.id)
-        new_posts += 1
-
-    print(f"\n✅ Found {new_posts} new posts.")
+    bot.run()
 
 
 if __name__ == "__main__":
