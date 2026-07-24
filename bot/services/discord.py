27d67fec9c566c9e818cc9466e3ce0b4982e5a83
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-25 01:55:05 +0530

    Update discord.py

--- bot/services/discord.py
+++ bot/services/discord.py
@@ -1,1 +1,12 @@
+from discord_webhook import DiscordWebhook
 
+from bot.config import DISCORD_WEBHOOK
+
+
+def send_message(message: str):
+    webhook = DiscordWebhook(
+        url=DISCORD_WEBHOOK,
+        content=message
+    )
+
+    webhook.execute()
