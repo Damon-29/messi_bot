187b9904eb73cabe3b5843b327883d0f655c4963
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-25 03:08:03 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,12 @@
 import os
+from pathlib import Path
+
 import yaml
 
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
+ROOT_DIR = Path(__file__).resolve().parent.parent
+CONFIG_PATH = ROOT_DIR / "config.yaml"
 
-with open("config.yaml", "r", encoding="utf-8") as f:
+with CONFIG_PATH.open("r", encoding="utf-8") as f:
     CONFIG = yaml.safe_load(f)
